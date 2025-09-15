bf72dab17377a1e942a44fce76cb1c570f8c373c
Alexander <alexanderm.kodehode@gmail.com>
2025-09-15 12:21:02 +0200

    worked on vectors, still missing decent brownie

--- src/components/Dish.jsx
+++ src/components/Dish.jsx
@@ -1,8 +1,24 @@
 import styles from "./Dish.module.css";
 export default function Dish({ rett }) {
-  //   const imageUrl = `https://source.unsplash.com/featured/300x200/?${rett.tittel}` This is broken, even without language stuff;
+  //   const imageUrl = `https://source.unsplash.com/featured/300x200/?${rett.tittel}` This is broken, even without language stuff; Using manual vectors instead
+  const slugify = (str) =>
+    str
+      .normalize("NFD")
+      .replace(/[\u0300-\u036f]/g, "")
+      .toLowerCase()
+      .replace(/æ/g, "ae")
+      .replace(/ø/g, "o")
+      .replace(/å/g, "a")
+      .replace(/\s+/g, "-")
+      .replace(/[^\w-]+/g, "");
+  const iconPath = `/icons/${slugify(rett.tittel)}.svg`;
   return (
     <div className={styles.card}>
+      <img
+        src={`${import.meta.env.BASE_URL}icons/${slugify(rett.tittel)}.svg`}
+        alt={rett.tittel}
+        className={styles.icon}
+      />
       <h2 className={styles.title}>{rett.tittel}</h2>
       <p className={styles.price}>Pris: {rett.pris}&thinsp;kr</p>
       <p className={styles.ingredients}>Ingredienser: {rett.ingredienser}</p>
